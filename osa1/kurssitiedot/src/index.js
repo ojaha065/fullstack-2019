052df41a1df8d06478bdcf18b59a8c03457a7eed
Jani Haiko <haiko.jani@gmail.com>
2019-05-28 14:40:35 +0300

    Kurssitiedot, step4

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -20,29 +20,31 @@ const Part = (props) => {
 
 const Total = (props) => {
     //console.log(props.exercises[0]);
-    return (<p>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>);
+    return (<p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>);
 };
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = {
-      name: 'Fundamentals of React',
-      exercises: 10
-    }
-    const part2 = {
-      name: 'Using props to pass data',
-      exercises: 7
-    }
-    const part3 = {
-      name: 'State of a component',
-      exercises: 14
-    }
+    const parts = [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
 
     return (
         <div>
             <Header course={course} />
-            <Content parts={[part1,part2,part3]} />
-            <Total exercises={[part1.exercises,part2.exercises,part3.exercises]} />
+            <Content parts={parts} />
+            <Total parts={parts} />
         </div>
     );
 };
