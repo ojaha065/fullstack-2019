953960be05dbb1ec43ea18426c6d4ea642317444
Jani Haiko <haiko.jani@gmail.com>
2019-05-29 14:09:36 +0300

    Unicafe, step234

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,6 +1,28 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Statistics = (props) => {
+    const palautteitaYhteensa = props.palautteet[0] + props.palautteet[1] + props.palautteet[2];
+
+    if(palautteitaYhteensa > 0){
+        return (
+            <div>
+                <p>Hyvä {props.palautteet[0]}</p>
+                <p>Neutraali {props.palautteet[1]}</p>
+                <p>Huono {props.palautteet[2]}</p>
+                <br />
+                <p>Palautteita yhteensä {palautteitaYhteensa}</p>
+    
+                <p>Keskiarvo {(props.palautteet[0] - props.palautteet[2]) / palautteitaYhteensa}</p>
+                <p>Positiivisia palautteita {props.palautteet[0] * 100 / palautteitaYhteensa} %</p>
+            </div>
+        );
+    }
+    else{
+        return (<p>Yhtään palautetta ei ole annettu.</p>);
+    }
+};
+
 const App = () => {
     const [good,setGood] = useState(0);
     const [neutral,setNeutral] = useState(0);
@@ -15,9 +37,7 @@ const App = () => {
             <button type="button" onClick={() => setBad(bad + 1)}>Huono</button>
 
             <h2>Tilastot</h2>
-            <p>Hyvä {good}</p>
-            <p>Neutraali {neutral}</p>
-            <p>Huono {bad}</p>
+            <Statistics palautteet={[good,neutral,bad]} />
         </div>
     );
 };
