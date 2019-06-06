e6c5eef74a54ff0255e1f71701ec81c073a4a7c2
Jani Haiko <haiko.jani@gmail.com>
2019-06-06 15:59:31 +0300

    Osan 1 palautus

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
 const Header = (props) => {
     return (<h1>{props.courseName}</h1>);
@@ -25,30 +25,30 @@ const Total = (props) => {
 
 const App = () => {
     const course = {
-        name: 'Half Stack application development',
-        parts: [
-          {
-            name: 'Fundamentals of React',
-            exercises: 10
-          },
-          {
-            name: 'Using props to pass data',
-            exercises: 7
-          },
-          {
-            name: 'State of a component',
-            exercises: 14
-          }
-        ]
-      }
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
+    };
 
     return (
-        <div>
-            <Header courseName={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </div>
+      <div>
+        <Header courseName={course.name} />
+        <Content parts={course.parts} />
+        <Total parts={course.parts} />
+      </div>
     );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />,document.getElementById("root"));
